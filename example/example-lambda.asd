f4d3439a750e6530ab4293035312ee2f507a8f86
Markus Klink <justjoheinz@gmail.com>
2023-12-20 17:13:30 +0100

    Test files

--- example/example-lambda.asd
+++ example/example-lambda.asd
@@ -1,5 +1,19 @@
 (defsystem example-lambda
   :defsystem-depends-on ("cl-aws-lambda/asdf")
   :class :lambda-system
+  :depends-on ("vom"
+               "vom-json"
+               "ningle"
+               "assoc-utils")
   :serial t
-  :components ((:file "hello")))
+  :components ((:file "hello"))
+  :in-order-to ((test-op (test-op "example-lambda/tests"))))
+
+
+(defsystem example-lambda/tests
+  :depends-on ("example-lambda"
+               "rove")
+  :serial t
+  :components ((:module "t"
+                :components ((:file "test"))))
+  :perform (test-op (op c) (symbol-call :rove '#:run c)))
--- example/hello.lisp
+++ example/hello.lisp
@@ -1,22 +1,48 @@
+(in-package :cl-user)
+
 (uiop:define-package example-lambda
-  (:use :cl)
-  (:import-from :cl-aws-lambda/runtime-interface
-   :request-id-of
-                :*context*)
+  (:use #:cl)
+  (:import-from #:ningle
+                #:app
+                #:route
+                #:make-request
+                #:make-response)
+  (:import-from #:cl-aws-lambda/runtime-interface
+                #:request-id-of
+                #:*context*)
+  (:local-nicknames (#:log #:vom))
   (:export #:hello))
 
 (in-package :example-lambda)
 
+(log:config t :debug)
+
 (declaim (optimize (space 3) (speed 0)))
+
+;; (defvar *app* (make-instance 'ningle:app))
+
+;; (setf (ningle:route *app* "/")
+;;       "Welcome to lambda!")
+
+;; (setf (ningle:route *app* "/login" :method :POST)
+;;       "POST Request to /login")
+
 
 (defmethod request-id-of ((object t))
   "some-id")
 
+;; (defmethod make-request ((app app)  env)
+;;   )
+;;
+
 (defun hello (event)
-  (let ((name (cdr (assoc "name" event :test #'string=))))
-    (if name
-        `( ("statusCode" . 200)
-           ("body" .
-                   (("response" . ,(format nil "Hello, ~a!" name))
-                    ("request-id" . ,(request-id-of *context*)))))
-        (error "No name supplied!"))))
+
+  (vom-json:with-json-logging
+      (log:info "event ~s" event)
+    (let ((name (cdr (assoc "name" event :test #'string=))))
+      (if name
+          `( ("statusCode" . 200)
+             ("body" .
+                     (("response" . ,(format nil "Hello, ~a!" name))
+                      ("request-id" . ,(request-id-of *context*)))))
+          (error "No name supplied!")))))
